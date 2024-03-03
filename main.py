51e3e2a9e74d05b9f4bcad93c11502cfdd95c287
Joas120 <157219358+Joas120@users.noreply.github.com>
2024-03-03 13:50:47 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,116 @@
+import sys
+
+#a = 56
+#print(a)
+#print(type(a))
+#b = 5.5
+#print(b)
+#print(type(b))
+#zmienna_tekstowa = 'wizualizacja danych'
+#print(zmienna_tekstowa)
+
+#print(type(zmienna_tekstowa))
+
+#a = 6.55
+#b = 3
+
+#c = a + b
+#print(c)
+#d = a - b
+#print(d)
+#e = 4
+#f = b // a
+#print(f)
+#f = a // b
+#print(f)
+#g = a ** 2
+#print(g)
+#h = pow(a, 2)
+#print(h)
+#
+#i = 6 ** (1/2)
+#j = pow(6, 1/2)
+#print(i)
+#print(j)
+#
+#k = 'wizualizacja danych'
+#l = ' grupa '
+#m = 1
+#print(k + l + str(m))
+#print('liczba a jest rowna {:.2f}, liczba b jest rowna {:d}'
+#      .format(*args: a, b))
+
+#a = input('Wprowadz liczbe: ')
+#print(a)
+#print(type(a))
+# a = int(a)
+#print(a*5)
+#print(type(a))
+
+#sys.stdout.write('Wprowadz liczbe: ')
+#b = sys.stdin.readline()
+#print(b)
+#print(type(b))
+
+#print(a + 'to wartosc liczby a')
+#print(type(a))
+#a = int(a)
+#print(a*5)
+#print(type(a))
+
+#sys.stdout.write('Wprowadz liczbe: ')
+#b = sys
+
+#lista = [5, 6.6, 34, 'a', 'b', [2, 3, 4], 'ab']
+#print(lista)
+#lista.append(67)
+#print(lista)
+#lista.insert(_index:2, _object: 'c')
+#print(lista)
+#lista.extend([20, 21, 22])
+#print(lista)
+#lista.pop()
+#print(lista)
+#lista.pop(2)
+#print(lista)
+#lista.remove([2, 3, 4])
+#print(lista)
+#del lista[1]
+#print(lista)
+#del lista
+#print(lista)
+#lista.reverse()
+#print(lista)
+#lista.sort()
+#print(lista)
+#
+#slownik = {'klucz': 'wartosc', 1: 2, 'a': 5, 4: 'b'}
+#print(slownik)
+#print(slownik[4])
+#slownik[6] = 45
+#print(slownik)
+#slownik.pop(1)
+#print(slownik)
+#print(slownik.keys())
+#print(slownik.values())
+#del slownik[6]
+
+#a = 8
+#b = 8
+
+#if a > b:
+#    print("a is greater than b")
+#elif a < b:
+#    print("a is less than b")
+#else:
+#    print("a is equal to b")
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if (a > b) & (c > d):
+    print(a, c)
+else:
+    print(b, d)
